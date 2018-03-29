7c4d64fe88d0efc7f0b04623fd1760872dcf6dec
Adolpho Noguera <adolph518@gmail.com>
2018-03-29 00:52:52 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -8,13 +8,8 @@ function forLoop(array){
 }
 
 function whileLoop(n){
-  while (n > -1 ){
+  while (n > 1){
     console.log(--n);
-    if (n === -1 ){
-      console.log("done");
-    }
-    else
-      console.log("done");
   }
 }
 
